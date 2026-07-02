a85830cad073499994f42ecafd5f33ea531b1232
aarizcoding13 <aarizhassankh@gmail.com>
2026-07-02 13:00:51 +0200

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,14 @@
+tup1 = (4, 3, 2, 2, -1, 18)
+tup2 = (2, 4, 8, 8, 3, 2, 9)
+
+total1 = 1
+for number in tup1:
+    total1 = total1 * number
+
+total2 = 1
+for number in tup2:
+    total2 = total2 * number
+
+print("Prouuduct of tup1:", total1)
+print("Productt of tup2:", total2)
+
